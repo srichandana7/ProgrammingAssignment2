1de0115d71ee72328aa78664e22e17e78324f67b
unknown <SSCR@PBY086P.ap.ds.monsanto.com>
2015-11-20 13:02:50 +0530

    Chan

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
 
 }
 
@@ -12,4 +13,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  
 }
+
+
+t<-matrix(4:7,2,2)
+t
+solve(t)
+
+u<-makeCacheMatrix(t)
+cacheSolve(u)
